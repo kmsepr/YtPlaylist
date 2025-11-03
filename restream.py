8ec1582b4258f3be1a32c340f32a04b56358cc57
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-11-03 19:58:18 +0530

    Update restream.py

--- restream.py
+++ restream.py
@@ -86,4 +86,4 @@ def convert():
         return f"<pre>❌ Error:\n{e.stderr.decode(errors='ignore')}</pre>", 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8000)
